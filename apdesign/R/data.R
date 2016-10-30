73923dea18520bf685c7dc451cd5e4b7fe8c235a
Tyler Matta <tyler.matta1@gmail.com>
2016-10-30 14:21:05 +0100

    adding doc files

---
+++ apdesign/R/data.R
@@ -0,0 +1,14 @@
+#' Longitudinal test scores over three school years.
+#'
+#' A dataset longitudinal test scores for students over three years.
+#'
+#' @format A data frame with 234 rows and 6 variables:
+#' \describe{
+#'   \item{id}{student identifier}
+#'   \item{cycle}{cycle number}
+#'   \item{grd}{grade level, corresponds to grade}
+#'   \item{season}{season within cycle/grade}
+#'   \item{week}{week since start of the cycle}
+#'   \item{score}{test score}
+#' }
+"three_season_cycle"
---
+++ apdesign/R/fun_apdesign.R
@@ -0,0 +1,37 @@
+#==============================================================================#
+#' apdesign: AP coding
+#' \code{apdesign} returns a data frame with additive polynomial coding
+#' 
+#' @param id_var A character that indicates the subject identifier in 
+#' \code{data}.
+#' @inheritParams apdesign_i
+#' 
+#' @return Output will be a data frame.
+#==============================================================================#
+#--- BEGIN apdesign -----------------------------------------------------------#
+apdesign <- function(data, 
+                     id_var, 
+                     time_var, 
+                     center_time, 
+                     cycle_var, 
+                     center_cycle, 
+                     max_degree = c(1, 1)){
+
+  df_split <- split(data, as.factor(data[, paste(id_var)]))
+
+  df_ap_split <- lapply(df_split, 
+                   function(x) apdesign_i(data         = x, 
+                                          time_var     = time_var, 
+                                          cycle_var    = cycle_var,
+                                          center_time  = center_time,
+                                          center_cycle = center_cycle,
+                                          max_degree   = max_degree,
+                                          matricies    = FALSE)
+                        )
+    
+  df_ap <- do.call(rbind, df_ap_split)
+  rownames(df_ap) <- NULL
+  return(df_ap)
+}
+#--- END apdesign -------------------------------------------------------------#
+#==============================================================================#
